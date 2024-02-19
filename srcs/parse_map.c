57eb5d9ec91bab69b0ab5918530fff869da22595
Mariia Kulikova <mkulikov@c4b12c4.42berlin.de>
2024-02-19 12:01:29 +0100

    Revert "refactor parse_line"
    This reverts commit b8c40b82df3f2c44bea55195b4c7f4861f8b83b4.

--- srcs/parse_map.c
+++ srcs/parse_map.c
@@ -6,38 +6,37 @@
 /*   By: mkulikov <mkulikov@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 12:19:39 by mkulikov          #+#    #+#             */
-/*   Updated: 2024/02/19 11:14:42 by mkulikov         ###   ########.fr       */
+/*   Updated: 2024/02/18 17:14:31 by mkulikov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static void	parse_line(char *line, t_params *params)
+static void	*parse_line(char *line, t_params *params)
 {
-	char		**arr;
-	int			x;
-	t_list		*new;
-	t_dot_param	*dot;
+	char	**splited;
+	int		x;
+	t_list	*new;
 
-	arr = ft_split(line, ' ');
-	if (!arr)
-		exit(1);
+	splited = ft_split(line, ' ');
+	if (!splited)
+		return (NULL);
 	x= 0;
-	while (*(arr + x))
+	while (*(splited + x))
 	{
-		dot = new_dot(x, params->map_height, (float)ft_atoi(*(arr + x)));
-		new = ft_lstnew(new_dot);
+		new = ft_lstnew(new_dot(x, params->map_height, (float)ft_atoi(*(splited + x++))));
 		if (!params->last_dot)
 			params->map = new;
 		else
 			params->last_dot->next = new;
 		params->last_dot = new;
-		x++;
 	}
+	// free(new);
 	params->map_width = x;
-	while (*(arr + --x))
-		free(*(arr + x));
-	free(arr);
+	x = 0;
+	while (*(splited + x))
+		free(*(splited + x++));
+	free(splited);
 }
 
 int	parse_map(int fd, t_params *params)
@@ -54,6 +53,7 @@ int	parse_map(int fd, t_params *params)
 		params->map_height += 1;
 		free(line);
 	}
+	// free(line);
 	printf("map width - %ld, height - %ld\n", params->map_width, params->map_height);
 	test(params);
 	return (0);
